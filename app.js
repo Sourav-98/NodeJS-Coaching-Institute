11e428e498edc986928c73b9511198154f17a164
Sourav-98 <sourav.raychaudhuri@gmail.com>
2020-02-26 00:35:33 +0530

    Added Add course page for admin

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ mongoose.connect('mongodb+srv://Sourav_98:Sourav1998$@cluster0-12p2n.mongodb.net
 
 
     
-    app.listen(9000);
+
 })
 .catch((err)=>{
     
@@ -120,7 +120,8 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use('/', DefaultRoutes);
-
+app.use('/admin', AdminRoutes);
+app.listen(9000);
 // Adding a new course
 // Course.create({course_name: "MongoDB Master Class", course_type:"Training", lec_hours: 35, max_seats: 25, price: 400}, (err, callback)=>{
 //     if(err){
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,11 @@
-// const express = ;
-const routes = require('express').Router();
+const express = require('express');
+const routes = express.Router();
+const path = require('path');
 
-routes.get('/', (req, res)=>{
+routes.use(express.static(path.join(__dirname, '..', 'public')));
 
+routes.get('/add-course', (req, res)=>{
+    res.render('admin/add-course', {pageTitle:"Add New Course", pagePath:'/admin/add-course'});
 });
 
 module.exports = routes;
