df7212e900cda1f777c0478aed2e3e6221b7a5f1
Sourav-98 <sourav.raychaudhuri@gmail.com>
2020-03-01 21:14:11 +0530

    file upload system uploaded

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,28 @@
 // contains controllers for admin for managing the available courses
 const Course = require('./../models/courseModel');
 
+exports.getAddCourse = (req, res)=>{
+    res.render('admin/add-course', {pageTitle:"Add New Course", pagePath:'/admin/add-course'});
+}
 
 exports.postAddCourse = (req, res)=>{
     var data = req.body;
-    
+    console.log(req.file);
+    res.redirect('/admin/add-course');
+    // Course.create({
+    //     course_name: data.course_name, 
+    //     course_type: data.course_type, 
+    //     course_author: data.course_author, 
+    //     lec_hours: data.lec_hours,
+    //     price: data.price,
+    //     max_seats: data.max_seats
+    // }, (err, callback)=>{
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     else{
+    //         console.log('Entry Added!');
+    //     }
+    //     res.redirect('/admin/home');
+    // });
 }
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const routes = express.Router();
 const path = require('path');
+const multer = require('multer');
+
+const coverImageStorage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'public/assets/coverImages');
+    },
+    filename: (req, file, cb)=>{
+        cb(null, new Date().toISOString()+'-'+file.originalname);
+    }
+});
+
+
+const adminController = require('./../controllers/adminController');
 
 routes.use(express.static(path.join(__dirname, '..', 'public')));
 
-routes.get('/add-course', (req, res)=>{
-    res.render('admin/add-course', {pageTitle:"Add New Course", pagePath:'/admin/add-course'});
-});
+routes.get('/add-course', adminController.getAddCourse);
+
+// routes.use(multer().single('coverImage'));
+routes.post('/add-course', multer({storage: coverImageStorage}).single('coverImage'), adminController.postAddCourse);
 
 module.exports = routes;
