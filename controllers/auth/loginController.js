b1c4ac943cd51ccc835b77351933b0d23c265b03
Sourav-98 <sourav.raychaudhuri@gmail.com>
2020-03-04 10:50:31 +0530

    Added Basic Route Protection

--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -4,51 +4,67 @@ const Admin = require('./../../models/adminModel');
 const bcrypt = require('bcrypt');
 
 exports.getLoginPage = (req, res)=>{
-    console.log(req.session.isLoggedIn);
-    res.render('auth/login', {pageTitle: "Login", pagePath:"/login", pass_err: false});
+    // console.log(req.session.isLoggedIn);
+    if(req.session.isLoggedIn == true){
+        res.redirect('/');
+    }
+    else{
+        res.render('auth/login', {pageTitle: "Login", pagePath:"/login", pass_err: false});
+    }
+    
 }
 
 exports.getAdminLoginPage = (req, res)=>{
-    res.render('admin/login', {pageTitle: "Admin Login", pagePath:"/admin-login", pass_err: pass_err});
+    if(req.session.isLoggedIn == true){
+        res.redirect('/');
+    }
+    else{
+        res.render('admin/login', {pageTitle: "Admin Login", pagePath:"/admin-login", pass_err: pass_err});
+    }
 }
 
 exports.postLogin = (req, res)=>{       // student login post control
-    var data = req.body;
-    Student.findOne({email_id: data.email_id}, (err, student)=>{
-        if(err){
-            console.log(err);
-            res.redirect('/');
-            return;
-        }
-        else{
-            if(student == undefined){
-                res.redirect('/signup');    // the user doesn't exist, hence reroute to signup page
+    if(req.session.isLoggedIn == true){
+        res.redirect('/');
+    }
+    else{
+        var data = req.body;
+        Student.findOne({email_id: data.email_id}, (err, student)=>{
+            if(err){
+                console.log(err);
+                res.redirect('/');
+                return;
             }
             else{
-                console.log(student);
-                bcrypt.compare(data.password, student.password, (err, match)=>{
-                    if(err){
-                        // console.log(err);
-                        res.redirect('/');
-                        return;
-                    }
-                    else{
-                        if(match == false){
-                            res.render('auth/login', {pageTitle: "Login", pagePath: "/login", pass_err: true});
-                            return;
-                        }
-                        else{
-                            req.session.isLoggedIn = true;
-                            req.session.mode = "student";
-                            req.session.user_id = student._id;
+                if(student == undefined){
+                    res.redirect('/signup');    // the user doesn't exist, hence reroute to signup page
+                }
+                else{
+                    console.log(student);
+                    bcrypt.compare(data.password, student.password, (err, match)=>{
+                        if(err){
+                            // console.log(err);
                             res.redirect('/');
                             return;
                         }
-                    }
-                });
+                        else{
+                            if(match == false){
+                                res.render('auth/login', {pageTitle: "Login", pagePath: "/login", pass_err: true});
+                                return;
+                            }
+                            else{
+                                req.session.isLoggedIn = true;
+                                req.session.mode = "student";
+                                req.session.user_id = student._id;
+                                res.redirect('/');
+                                return;
+                            }
+                        }
+                    });
+                }
             }
-        }
-    });    
+        });
+    }  
 }
 
 exports.postAdminLogin = (req, res)=>{  // admin login post control
@@ -58,6 +74,11 @@ exports.postAdminLogin = (req, res)=>{  // admin login post control
 }
 
 exports.getLogout = (req, res)=>{
-    req.session.destroy();  // destroy the session after logging out
-    res.redirect('/');
+    if(req.session.isLoggedIn==false){
+        res.redirect('/');
+    }
+    else{
+        req.session.destroy();  // destroy the session after logging out
+        res.redirect('/');
+    }
 }
--- controllers/auth/signupController.js
+++ controllers/auth/signupController.js
@@ -2,59 +2,68 @@ const Student = require('./../../models/studentModel');
 const bcrypt = require('bcrypt');
 
 exports.getSignupPage = (req, res)=>{
-    res.render('auth/signup', {pageTitle: "Signup", pagePath:"/signup", err: {email_match: undefined, 
-        password_match: undefined}});
+    if(req.session.isLoggedIn == true){
+        res.redirect('/');
+    }
+    else{
+        res.render('auth/signup', {pageTitle: "Signup", pagePath:"/signup", err: {email_match: undefined, 
+            password_match: undefined}});
+    }
 }
 
 exports.postSignup = (req, res)=>{
-    // register a new user
-    var data = req.body;
-    // console.log(data);
-    Student.findOne({email_id: data.email_id}, (err, student)=>{
-        if(err){
-            console.log(err);
-            // res.render();   // server error - 500
-        }
-        else{
-            var error = {
-                email_match: undefined, 
-                password_match: undefined
-            };
-
-            if( student != undefined){
-                error.email_match = true;
-                res.render('auth/signup', {pageTitle: "Signup", pagePath:"/signup", err: error});
-                return;
+    if(req.session.isLoggedIn == true){
+        res.redirect('/');
+    }
+    else{
+        // register a new user
+        var data = req.body;
+        // console.log(data);
+        Student.findOne({email_id: data.email_id}, (err, student)=>{
+            if(err){
+                console.log(err);
+                // res.render();   // server error - 500
             }
             else{
-                if(data.password != data.confirmPassword){
-                    error.password_match = false;
+                var error = {
+                    email_match: undefined, 
+                    password_match: undefined
+                };
+                if( student != undefined){
+                    error.email_match = true;
                     res.render('auth/signup', {pageTitle: "Signup", pagePath:"/signup", err: error});
                     return;
                 }
-                bcrypt.hash(data.password, 10, (err, hash_password)=>{
-                    if(err){
-                        res.redirect('/signup');
+                else{
+                    if(data.password != data.confirmPassword){
+                        error.password_match = false;
+                        res.render('auth/signup', {pageTitle: "Signup", pagePath:"/signup", err: error});
                         return;
                     }
-                    else{
-                        Student.create({
-                            name: data.name,
-                            email_id: data.email_id,
-                            password: hash_password
-                        }, 
-                        (err, student)=>{
-                            if(err){
-                                console.log(err);
-                                res.redirect('/');
-                            }
-                            else{
-                                res.redirect('/login');
-                            }
-                        });
-                    }
-                });
+                    bcrypt.hash(data.password, 10, (err, hash_password)=>{
+                        if(err){
+                            res.redirect('/signup');
+                            return;
+                        }
+                        else{
+                            Student.create({
+                                name: data.name,
+                                email_id: data.email_id,
+                                password: hash_password
+                            }, 
+                            (err, student)=>{
+                                if(err){
+                                    console.log(err);
+                                    res.redirect('/');
+                                }
+                                else{
+                                    res.redirect('/login');
+                                }
+                            });
+                        }
+                    });
+                }
             }
-        }
-    });
+        });
+    }
 }
