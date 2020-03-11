d313541ce7de68f6360146f6afe18a0ffbf5b8d7
Sourav-98 <sourav.raychaudhuri@gmail.com>
2020-03-11 09:52:12 +0530

    Updated Course Details and Course List Page

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -4,12 +4,29 @@ const Student = require('./../models/studentModel');
 const Course = require('./../models/courseModel');
 
 exports.getCoursesPage = (req, res)=>{
-    res.render('course/course-list', {pageTitle: "Courses", pagePath: "/courses", session_data: req.session});
+    var course_list = undefined;
+    Course.find((err, courses)=>{
+        if(err){
+            res.redirect('/admin');
+            return;
+        }
+        else{
+            res.render('course/course-list', {pageTitle: "Courses", pagePath: "/courses", session_data: req.session, courses: courses});
+        }
+    });
 }
 
 exports.getCourseInfoPage = (req, res)=>{
     var id = req.params.course_id;
-    res.render('course/course-details', {pageTitle: "Courses", pagePath: "/courses", course_id: id, session_data: req.session});
+    Course.findOne({_id: id}, (err, course)=>{
+        if(err){
+            console.log(err);
+            res.redirect('/courses');
+        }
+        else{
+            res.render('course/course-details', {pageTitle: "Courses", pagePath: "/courses", course: course, session_data: req.session});
+        }
+    });
 }
 
 exports.getMyCoursesPage = (req, res)=>{
