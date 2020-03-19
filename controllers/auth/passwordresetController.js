53ddd6014eff9579e249a818c2ba8966cdc5e303
Sourav-98 <sourav.raychaudhuri@gmail.com>
2020-03-19 20:24:16 +0530

    Updated Index Page

--- controllers/auth/passwordresetController.js
+++
@@ -1,8 +0,0 @@
-
-exports.getPasswordResetPage = (req, res)=>{
-    res.render('auth/password-reset', {pageTitle: "Password Reset", pagePath:"/password-reset"});
-}
-
-exports.postPasswordReset = (req, res)=>{
-    
-}
